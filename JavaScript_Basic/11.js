923385cc29709f63dbf9fcc18e826dff4db98e59
Shih-chen Chao <cybersagittarius@gmail.com>
2020-09-03 11:57:48 +0200

    finished solution to exercise 11

---
+++ JavaScript_Basic/11.js
@@ -0,0 +1,34 @@
+//## 1. Minimum and maximum
+//### a. Lowest Number
+const aList=[-1, 0, 1, 2, 3, 4];
+console.log(aList);
+
+//### b. Highest Number
+console.log(Math.max(...aList));
+
+//## 2. Rounding
+//### a. Round up
+//Round up the following numbers: 3321.32321, 326.76, 76788.7, -9.78, 43.342.
+const bList=[3321.32321, 326.76, 76788.7, -9.78, 43.342];
+console.log(bList);
+var i;
+for(i=0; i<bList.length; i++) {
+    console.log("The number is", Math.round(bList[i]), "\n"); 
+}
+//### b. Round down
+//Round down the following numbers: 3321.32321, 326.76, 76788.7, -9.78, 28.329.
+var j;
+for(j=0; j<bList.length; j++) {
+    console.log("The number is",Math.floor(bList[j]), "\n"); 
+}
+//## 3. Dice Roll!
+//Create a program that prints a random **_integer_** from 1 - 6.
+function myDice () {
+    console.log(Math.floor(Math.random()*6)+1);
+}
+
+myDice();
+
+
+
+
